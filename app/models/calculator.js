f91521c5beba955de103514585db8c5c4546ff96
Kristian Kanya <kriskanya@gmail.com>
2014-06-02 14:37:23 -0500

    calculator

--- app/models/calculator.js
+++ app/models/calculator.js
@@ -26,31 +26,40 @@ class Calculator{
     return sumArray;
   }
 
-  static fractionAdd(objs){
-    var fractionSum = objs;  //{x:5, y:4}, {x:2, y:6}
-    console.log('xxxx');
-    console.log(objs[0].x);
-    console.log('yyyyy');
-    console.log(objs[1].y);
-    for(var i = 0; i < objs.length; i++){
-      var denom = 1;
-      console.log('-------');
-      denom *= (objs[i].y);
-      console.log('-------');
-      // (objs[0].y) * (objs[1].y) * (objs[2].y);
-    }
+  // static fractionAdd(objs){
+  //   var fractionSum = objs;  //{x:5, y:4}, {x:2, y:6}
+  //   console.log('xxxx');
+  //   console.log(objs[0].x);
+  //   console.log('yyyyy');
+  //   console.log(objs[1].y);
+  //   for(var i = 0; i < objs.length; i++){
+  //     var denom = 1;
+  //     console.log('-------');
+  //     denom *= (objs[i].y);
+  //     console.log('-------');
+  //     // (objs[0].y) * (objs[1].y) * (objs[2].y);
+  //   }
 
 
-    console.log(fractionSum);
-  }
-  //
-  // static addTwoFractions(x, y){
-  //   var [nX, dX] = x.trim().split('/').map(n=>n*1);  //array desctructuring - numerator(n), denominator(d)
-  //   var [nY, dY] = y.trim().split('/').map(n=>n*1);
-  //
-  //   var nX1 = dY + nX;
-  //   var nY1 = dX + nY;
+  //   console.log(fractionSum);
   // }
+
+  static addTwoFractions(x, y){
+    var [nX, dX] = x.trim().split('/').map(n=>n*1);  //array desctructuring - numerator(n), denominator(d)
+    var [nY, dY] = y.trim().split('/').map(n=>n*1);
+
+    var nX1 = dY * nX;
+    var nY1 = dX * nY;
+
+    var numerator = nX1 + nY1;
+    var denominator = dX * dY;
+
+    return `${numerator}/${denominator}`;
+  }
+
+  static addFractions(...numbers){
+    return numbers.reduce((prev, curr)=>Calculator.addTwoFractions(prev,curr), numbers.pop());
+  }
 
 }
 
